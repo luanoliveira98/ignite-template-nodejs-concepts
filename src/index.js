d67f7f0294e72c5e986bb1ad8e7664410d36eb4a
Luan Oliveira <luanoliveira17@hotmail.com>
2022-12-16 22:06:34 -0300

    Fix needed to run tests

--- src/index.js
+++ src/index.js
@@ -70,12 +70,12 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   const todo = user.todos.find((todo) => todo.id === id);
-  if (!todo) return response.status(400).json({ error: "Todo not found!" });
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
 
   todo.title = title;
   todo.deadline = new Date(deadline);
 
-  return response.status(204).send();
+  return response.status(200).json(todo);
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
@@ -83,11 +83,11 @@ app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   const todo = user.todos.find((todo) => todo.id === id);
-  if (!todo) return response.status(400).json({ error: "Todo not found!" });
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
 
   todo.done = true;
 
-  return response.status(204).send();
+  return response.status(200).json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
@@ -95,7 +95,7 @@ app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
   const todo = user.todos.find((todo) => todo.id === id);
-  if (!todo) return response.status(400).json({ error: "Todo not found!" });
+  if (!todo) return response.status(404).json({ error: "Todo not found!" });
 
   user.todos.splice(todo, 1);
 

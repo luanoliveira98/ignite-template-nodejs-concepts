28f577e20bb525c2cb363332f7ba167ac775e3a7
Luan Oliveira <luanoliveira17@hotmail.com>
2022-12-16 21:39:59 -0300

    Update todo

--- src/index.js
+++ src/index.js
@@ -65,7 +65,17 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+  const { user } = request;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) return response.status(400).json({ error: "Todo not found!" });
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.status(204).send();
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
